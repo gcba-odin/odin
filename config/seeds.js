19ec62ca709e56a257162f139853ec4f87cba9c8
Rita Zerrizuela <zeta@widcket.com>
2016-05-18 16:36:41 -0300

    Added seeds.js

---
+++ config/seeds.js
@@ -0,0 +1,146 @@
+module.exports = {
+    fixtures: {
+        order: ['Users', 'Organizations', 'Categories'],
+        Users: [
+            {
+                id: 'dogPzIz8',
+                username: 'henrywagner',
+                password: '123',
+                email: 'hwagner0@merriam-webster.com',
+                firstName: 'Henry',
+                lastName: 'Wagner',
+                avatar: '',
+                active: true,
+                organization: '23TplPdS',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'nYrnfYEv',
+                username: 'howardfoster',
+                password: '123',
+                email: 'hfoster1@cbsnews.com',
+                firstName: 'Howard',
+                lastName: 'Foster',
+                avatar: '',
+                active: true,
+                organization: '23TplPdS',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'a4vhAoFG',
+                username: 'ashleychavez',
+                password: '123',
+                email: 'achavez2@go.com',
+                firstName: 'Ashley',
+                lastName: 'Chavez',
+                avatar: '',
+                active: true,
+                organization: '23TplPdS',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'hwX6aOr7',
+                username: 'melissafrazier',
+                password: '123',
+                email: 'mfrazier3@gizmodo.com',
+                firstName: 'Melissa',
+                lastName: 'Frazier',
+                avatar: '',
+                active: true,
+                organization: '23TplPdS',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'eWRhpRV',
+                username: 'petermorrison',
+                password: '123',
+                email: 'pmorrison4@wp.com',
+                firstName: 'Peter',
+                lastName: 'Morrison',
+                avatar: '',
+                active: true,
+                organization: '23TplPdS',
+                createdBy: 'dogPzIz8'
+            }
+        ],
+        Organizations: [
+            {
+                id: 'eWRhpRV',
+                name: 'ACME',
+                description: 'An example organization.',
+                address: 'Example Ave. 254',
+                active: true,
+                parent: undefined,
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'dWRhpRV',
+                name: 'Umbrella Corp',
+                description: 'An example organization.',
+                address: 'Example Ave. 255',
+                active: true,
+                parent: undefined,
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'fWRhpRV',
+                name: 'Star Labs',
+                description: 'An example organization.',
+                address: 'Example Ave. 256',
+                active: true,
+                parent: undefined,
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'gWRhpRV',
+                name: 'Aperture Labs',
+                description: 'An example organization.',
+                address: 'Example Ave. 257',
+                active: true,
+                parent: undefined,
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'hWRhpRV',
+                name: 'Oscorp',
+                description: 'An example organization.',
+                address: 'Example Ave. 258',
+                active: true,
+                parent: undefined,
+                createdBy: 'dogPzIz8'
+            }  
+        ],
+        Categories: [
+            {
+                id: 'iWRhpRV',
+                name: 'Art',
+                description: 'An example category.',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'jWRhpRV',
+                name: 'Science',
+                description: 'An example category.',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'kWRhpRV',
+                name: 'Public Health',
+                description: 'An example category.',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'lWRhpRV',
+                name: 'Technology',
+                description: 'An example category.',
+                createdBy: 'dogPzIz8'
+            },
+            {
+                id: 'mWRhpRV',
+                name: 'Other',
+                description: 'An example category.',
+                createdBy: 'dogPzIz8'
+            }
+        ]
+    }
+}
