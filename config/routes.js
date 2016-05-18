1deb535e27cd48effcf154c825d9f207c7a3beb5
Ariel Leguizamon <ariel.leguizamon@gmail.com>
2016-05-18 17:18:39 -0300

    User routes

--- config/routes.js
+++ config/routes.js
@@ -8,6 +8,21 @@
 
 module.exports = {
     routes: {
-      'PATCH /users/:id' : { blueprint: 'update', model: 'user' },
+        'PATCH /users/:id' : { blueprint: 'update', model: 'user' },
+        'HEAD /users',
+        'OPTIONS /users',
+        'GET /users/first',
+        'HEAD /users/first',
+        'OPTIONS /users/first',
+        'GET /users/last',
+        'HEAD /users/last',
+        'OPTIONS /users/last',
+        'GET /users/count',
+        'HEAD /users/count',
+        'OPTIONS /users/count',
+        'HEAD /users/:id',
+        'OPTIONS /users/:id',
+        'HEAD /users/login',
+        'OPTIONS /users/login',
     }
 };
