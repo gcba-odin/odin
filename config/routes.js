31c55af34a36eede81e2be999b327e6bafcfcf9d
Rita Zerrizuela <zeta@widcket.com>
2016-05-26 19:33:57 -0300

    Cleanup

--- config/routes.js
+++ config/routes.js
@@ -14,19 +14,6 @@ module.exports = {
         'POST /users/login': 'User.login', // LOGOUT missing!!!
         'GET /users/refreshToken': 'User.refreshToken',
 
-<<<<<<< HEAD
-        'GET /users/first': { blueprint: 'first', model: 'user' },
-        'HEAD /users/first': { controller: 'Head', action: 'Head', model: 'user'},
-
-        'GET /users/last': { blueprint: 'last', model: 'user' },
-        'HEAD /users/last': {controller: 'Head', action: 'Head', model: 'user'},
-
-        'PATCH /users/:id' : { blueprint: 'update', model: 'user' },
-        'HEAD /users/:id': { controller: 'Head', action: 'Head', model: 'user'},
-        // 'OPTIONS /users/:id': { controller : 'Option', model: 'user', action: 'collection' },
-
-        'OPTIONS /users': { controller : 'Options', model: 'user', action: 'collection' },
-=======
         'GET /users/first': {
             blueprint: 'first',
             model: 'user'
@@ -61,21 +48,10 @@ module.exports = {
             model: 'user',
             action: 'foo'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
 
 
         // Organizations
 
-<<<<<<< HEAD
-        'GET /organizations/first': { blueprint: 'first', model: 'organization' },
-        'HEAD /organizations/first': { controller: 'Head', action: 'Head', model: 'organization'},
-
-        'GET /organizations/last': { blueprint: 'last', model: 'organization' },
-        'HEAD /organizations/last': {controller: 'Head', action: 'Head', model: 'organization'},
-
-        'PATCH /organizations/:id' : { blueprint: 'update', model: 'organization' },
-        'HEAD /organizations/:id': { controller: 'Head', action: 'Head', model: 'organization'},
-=======
         'GET /organizations/first': {
             blueprint: 'first',
             model: 'organization'
@@ -105,22 +81,12 @@ module.exports = {
             action: 'Head',
             model: 'organization'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /organizations/:id': { blueprinst: 'update', model: 'organization' },
 
 
         // Categories
 
-<<<<<<< HEAD
-        'GET /categories/first': { blueprint: 'first', model: 'category' },
-        'HEAD /categories/first': { controller: 'Head', action: 'Head', model: 'category'},
-
-        'GET /categories/last': { blueprint: 'last', model: 'category' },
-        'HEAD /categories/last': {controller: 'Head', action: 'Head', model: 'category'},
-
-        'PATCH /categories/:id' : { blueprint: 'update', model: 'category' },
-        'HEAD /categories/:id': { controller: 'Head', action: 'Head', model: 'category'},
-=======
         'GET /categories/first': {
             blueprint: 'first',
             model: 'category'
@@ -150,22 +116,12 @@ module.exports = {
             action: 'Head',
             model: 'category'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /categories/:id': { blueprinst: 'update', model: 'category' },
 
 
         // Statuses
 
-<<<<<<< HEAD
-        'GET /statuses/first': { blueprint: 'first', model: 'status' },
-        'HEAD /statuses/first': { controller: 'Head', action: 'Head', model: 'status'},
-
-        'GET /statuses/last': { blueprint: 'last', model: 'status' },
-        'HEAD /statuses/last': {controller: 'Head', action: 'Head', model: 'status'},
-
-        'PATCH /statuses/:id' : { blueprint: 'update', model: 'status' },
-        'HEAD /statuses/:id': { controller: 'Head', action: 'Head', model: 'status'},
-=======
         'GET /statuses/first': {
             blueprint: 'first',
             model: 'status'
@@ -195,22 +151,12 @@ module.exports = {
             action: 'Head',
             model: 'status'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /statuses/:id': { blueprinst: 'update', model: 'status' },
 
 
         // Filetypes
 
-<<<<<<< HEAD
-        'GET /filetypes/first': { blueprint: 'first', model: 'filetype' },
-        'HEAD /filetypes/first': { controller: 'Head', action: 'Head', model: 'filetype'},
-
-        'GET /filetypes/last': { blueprint: 'last', model: 'filetype' },
-        'HEAD /filetypes/last': {controller: 'Head', action: 'Head', model: 'filetype'},
-
-        'PATCH /filetypes/:id' : { blueprint: 'update', model: 'filetype' },
-        'HEAD /filetypes/:id': { controller: 'Head', action: 'Head', model: 'filetype'},
-=======
         'GET /filetypes/first': {
             blueprint: 'first',
             model: 'filetype'
@@ -240,22 +186,12 @@ module.exports = {
             action: 'Head',
             model: 'filetype'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /filetypes/:id': { blueprinst: 'update', model: 'filetype' },
 
 
         // Files
 
-<<<<<<< HEAD
-        'GET /files/first': { blueprint: 'first', model: 'file' },
-        'HEAD /files/first': { controller: 'Head', action: 'Head', model: 'file'},
-
-        'GET /files/last': { blueprint: 'last', model: 'file' },
-        'HEAD /files/last': {controller: 'Head', action: 'Head', model: 'file'},
-
-        'PATCH /files/:id' : { blueprint: 'update', model: 'file' },
-        'HEAD /files/:id': { controller: 'Head', action: 'Head', model: 'file'},
-=======
         'GET /files/first': {
             blueprint: 'first',
             model: 'file'
@@ -285,22 +221,12 @@ module.exports = {
             action: 'Head',
             model: 'file'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /files/:id': { blueprinst: 'update', model: 'file' },
 
 
         // Databases
 
-<<<<<<< HEAD
-        'GET /databases/first': { blueprint: 'first', model: 'database' },
-        'HEAD /databases/first': { controller: 'Head', action: 'Head', model: 'database'},
-
-        'GET /databases/last': { blueprint: 'last', model: 'database' },
-        'HEAD /databases/last': {controller: 'Head', action: 'Head', model: 'database'},
-
-        'PATCH /databases/:id' : { blueprint: 'update', model: 'database' },
-        'HEAD /databases/:id': { controller: 'Head', action: 'Head', model: 'database'},
-=======
         'GET /databases/first': {
             blueprint: 'first',
             model: 'database'
@@ -330,28 +256,12 @@ module.exports = {
             action: 'Head',
             model: 'database'
         },
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
+
         // 'OPTIONS /databases/:id': { blueprinst: 'update', model: 'database' },
 
 
         // Datasets
 
-<<<<<<< HEAD
-        'GET /datasets/first': { blueprint: 'first', model: 'dataset' },
-        'HEAD /datasets/first': { controller: 'Head', action: 'Head', model: 'dataset'},
-
-        'GET /datasets/last': { blueprint: 'last', model: 'dataset' },
-        'HEAD /datasets/last': {controller: 'Head', action: 'Head', model: 'dataset'},
-
-        'PATCH /datasets/:id' : { blueprint: 'update', model: 'dataset' },
-        'HEAD /datasets/:id': { controller: 'Head', action: 'Head', model: 'dataset'},
-        // 'OPTIONS /datasets/:id': { blueprinst: 'update', model: 'dataset' },
-
-        // Files
-
-        'POST /files/upload': 'File.upload',
-        'GET /files/download/:filename': 'File.download'
-=======
         'GET /datasets/first': {
             blueprint: 'first',
             model: 'dataset'
@@ -481,6 +391,5 @@ module.exports = {
             action: 'Head',
             model: 'config'
         }
->>>>>>> 60880e099c3547eb1022c456e4945cdbecb62865
     }
 };
