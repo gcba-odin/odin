88907918d28598846f02aac4e1c7ee6f813a2c1a
Ariel Leguizamon <ariel.leguizamon@gmail.com>
2016-05-27 12:57:04 -0300

    changed data to methods

--- api/responses/options.js
+++ api/responses/options.js
@@ -3,10 +3,8 @@
 module.exports = function (data, meta) {
     const response = _.assign({
         meta: meta,
-        data: data
+        methods: data
     });
-    console.log('response es + ' + JSON.stringify(response));
-    // // Add headers to the res object as needed
     this.res.status(200);
     this.res.jsonx(response);
 };
