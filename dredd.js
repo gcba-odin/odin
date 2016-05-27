728e8893704bf274e744aae839b06f2fba5f4aa8
Rita Zerrizuela <zeta@widcket.com>
2016-05-27 19:44:19 -0300

    Added dredd hooks

--- dredd.js
+++ dredd.js
@@ -1,11 +1,42 @@
 var hooks = require('hooks');
-var before = hooks.before;
-var after = hooks.after;
-var responseStash = {};
 
-before("Users > Users > Create a User", function(transaction, done) {
-    transaction.fullPath.slice(0, -3) += "/" + 'dogPzIz9';
-    console.log('Entro al hook!!!!!!!!!!!!!!!!!');
-    console.dir(transaction);
-    done();
+// Recursively add null as acceptable type if there is string
+// "#nullable" present in the property description
+var patchPropertiesWithNullable = function(schema) {
+    if (typeof(schema['properties']) == 'object' && !Array.isArray(schema['properties'])) {
+        for (property in schema['properties']) {
+            var partialSchemaToPatch = schema['properties'][property];
+            schema['properties'][property] = patchPropertiesWithNullable(partialSchemaToPatch);
+        };
+    };
+
+    if (schema['description'] !== undefined) {
+        if (schema['description'].indexOf("#nullable") > -1) {
+            if (schema['type'] === undefined) {
+                schema['type'] = 'null';
+
+            } else if (typeof(schema['type']) == 'string') {
+                schema['type'] = [schema['type'], 'null'];
+
+            } else if (Array.isArray(schema['type'])) {
+                schema['type'].push('null');
+
+            };
+        };
+    };
+
+    return (schema);
+};
+
+hooks.beforeAll(function(transactions, callback) {
+    for (index in transactions) {
+        var transaction = transactions[index];
+        if (transaction['expected']['bodySchema'] !== undefined) {
+            var schema = JSON.parse(transaction['expected']['bodySchema']);
+            schema = patchPropertiesWithNullable(schema);
+            transactions[index]['expected']['bodySchema'] = JSON.stringify(schema, null, 2);
+        };
+    };
+
+    callback();
 });
