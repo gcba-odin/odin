30c29ec70e11345879a1b96f7747df43ae0f55eb
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-05-30 14:22:18 -0300

    changed file upload dest

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -8,27 +8,31 @@
 module.exports = {
 
     upload: function (req, res) {
+        console.log(req.file('uploadFile'));
         var uploadFile = req.file('uploadFile');
 
         var origifile = uploadFile._files[0].stream.filename;
 
+        console.log('error');
+        console.log('error');
+        console.log('error');
+        console.log('error');
+
         uploadFile.upload({
             saveAs: origifile,
-            dirname: require('path').resolve('~/files')
-            // dirname: require('path').resolve(sails.config.appPath + '/assets/csv/to-review')
+            // dirname: require('path').resolve(sails.config.appPath + '/files')
+            dirname: require('path').resolve('/home/Admin001/odin/files')
         }, function onUploadComplete(err, files) {
-            console.log(files[0].fd);
-
             if (err) return res.serverError(err);
             //	IF ERROR Return and send 500 error with error
 
-            res.json({status: 200, file: files});
+            res.json({status: 200});
         });
     },
     download: function (req, res) {
         var file = req.param('filename');
-        // var dirname = require('path').resolve(sails.config.appPath + '/assets/csv/to-review/' + file);
-        var dirname = require('path').resolve('~/files/' + file);
+        var dirname = require('path').resolve('/home/Admin001/odin/files/' + file);
+        // var dirname = require('path').resolve(sails.config.appPath + '/files/' + file);
         var SkipperDisk = require('skipper-disk');
         var fileAdapter = SkipperDisk();
         fileAdapter.read(dirname).on('error', function (err) {
