28dded5367a89a25a2290d497b8644ea508028c7
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-05-30 14:51:26 -0300

    message if noop file is uploaded

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -10,13 +10,14 @@ module.exports = {
     upload: function (req, res) {
         // console.log(req.file('uploadFile'));
         var uploadFile = req.file('uploadFile');
-
-        if (uploadFile._files[0]) {
-            var origifile = uploadFile._files[0].stream.filename;
+        console.log(uploadFile.isNoop);
+        if (uploadFile.isNoop) {
+            return res.badRequest('Uploaded File is Noop. No file was uploaded');
         }
+
+        var origifile = uploadFile._files[0].stream.filename;
         uploadFile.upload({
             saveAs: origifile,
-            // dirname: require('path').resolve(sails.config.appPath + '/files')
             dirname: require('path').resolve('/home/Admin001/odin/files')
         }, function onUploadComplete(err, files) {
             //	IF ERROR Return and send 500 error with error
@@ -30,7 +31,6 @@ module.exports = {
     download: function (req, res) {
         var file = req.param('filename');
         var dirname = require('path').resolve('/home/Admin001/odin/files/' + file);
-        // var dirname = require('path').resolve(sails.config.appPath + '/files/' + file);
         var SkipperDisk = require('skipper-disk');
         var fileAdapter = SkipperDisk();
         fileAdapter.read(dirname).on('error', function (err) {
