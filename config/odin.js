4fccfb3e0b4fea7a0285efd47b3d4c7f35b5c309
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-05-31 13:21:32 -0300

    added allowed filetypes

--- config/odin.js
+++ config/odin.js
@@ -1,5 +1,6 @@
 module.exports.odin = {
     uploadFolder: '/home/Admin001/files',
-    allowedTypes : ['text/yaml', 'image/jpeg', 'image/png'],
+    allowedTypes: ['text/yaml', 'text/csv', 'text/html', 'text/calendar', 'application/pdf', 'application/x-rar-compressed', 'application/rss+xml',
+        'application/octec-stream', 'application/vnd.ms-excel', 'application/xml', 'application/zip'],
     defaultEncoding: ''
 };
