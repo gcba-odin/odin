592c72dac9ace5dc5f52fa05defce23828f647fb
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-05-31 10:01:35 -0300

    working on upload files

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -8,31 +8,57 @@
 module.exports = {
 
     upload: function (req, res) {
-        var uploadFile = req.file('uploadFile');
-        if (uploadFile.isNoop) {
-            return res.badRequest('Uploaded File is Noop. No file was uploaded');
-        }
 
-        var origifile = uploadFile._files[0].stream.filename;
+        var uploadFile = req.file('uploadFile').on('error', function (err) {
+            console.log('Errror!!!!!!!!!!');
+            return res.negotiate(err);
+        });
+        
         uploadFile.upload({
-            saveAs: origifile,
-            dirname: require('path').resolve('/home/Admin001/files')
+            saveAs: uploadFile._files[0].stream.filename,
+            dirname: require('path').resolve(sails.config.odin.uploadFolder)
         }, function onUploadComplete(err, files) {
             //	IF ERROR Return and send 500 error with error
             if (err) return res.serverError(err);
             if (files.length === 0) {
                 return res.badRequest('No file was uploaded');
             }
-            res.json({status: 200});
-        });
+            res.json({
+                status: 200
+            });
+        })
     },
     download: function (req, res) {
         var file = req.param('filename');
-        var dirname = require('path').resolve('/home/Admin001/files/' + file);
+        // var dirname = require('path').resolve('/home/Admin001/files/' + file);
+        var dirname = require('path').resolve(sails.config.odin.uploadFolder + '/' + file);
         var SkipperDisk = require('skipper-disk');
         var fileAdapter = SkipperDisk();
         fileAdapter.read(dirname).on('error', function (err) {
             return res.serverError(err);
         }).pipe(res);
     }
-};
+    ,
+    index: function (req, res, next) {
+        var fs = require('fs');
+        var dirname = require('path').resolve('/home/lothorien/files/');
+        var files = {};
+        fs.readdir(dirname, function (err, filenames) {
+            if (err) {
+                next(err);
+            }
+            // filenames.forEach(function (filename) {
+            //   fs.readFile(dirname + '/' + filename, 'utf-8', function (err, content) {
+            //     if (err) {
+            //       return next(err);
+            //     }
+            //     // onFileContent(filename, content);
+            //   });
+            // });
+            return res.ok({files: filenames})
+        });
+    }
+    ,
+
+}
+;
