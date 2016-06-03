ad402b912a5d7a61f4ec5e685828d2cf4fde2ded
Rita Zerrizuela <zeta@widcket.com>
2016-06-03 16:27:15 -0300

    Changed settings

--- config/autoreload.js
+++ config/autoreload.js
@@ -4,8 +4,12 @@ module.exports.autoreload = {
     dirs: [
         "api/models",
         "api/controllers",
-        "api/services"
+        "api/services",
+        "api/blueprints",
+        "api/hooks",
+        "api/policies",
+        "api/responses"
     ],
-    overrideMigrateSetting: true,
+    overrideMigrateSetting: false,
     ignored: ['i18n']
 };
