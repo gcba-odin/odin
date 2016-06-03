f7bc0592b29ae9067fd7a9ec3390cd3400d5fbfd
Rita Zerrizuela <zeta@widcket.com>
2016-06-02 21:12:39 -0300

    Fixed typo

--- config/odin.js
+++ config/odin.js
@@ -1,6 +1,6 @@
 module.exports.odin = {
     uploadFolder: '/home/Admin001/files',
     allowedTypes: ['text/yaml', 'text/csv', 'text/html', 'text/calendar', 'application/pdf', 'application/x-rar-compressed', 'application/rss+xml',
-        'application/octec-stream', 'application/vnd.ms-excel', 'application/xml', 'application/zip'],
-    defaultEncoding: ''
+        'application/octet-stream', 'application/vnd.ms-excel', 'application/xml', 'application/zip'],
+    defaultEncoding: 'UTF-8' //TODO: Set UTF-8
 };
