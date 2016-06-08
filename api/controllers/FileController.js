715b53db5a9c410b3e00669c46f218d652f510d2
Rita Zerrizuela <zeta@widcket.com>
2016-06-08 18:35:07 -0300

    Restored file upload functionality

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -7,42 +7,19 @@
 const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
 var mime = require('mime');
 var shortid = require('shortid');
+var iconv = require('iconv-lite');
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({
     delimiter: 'auto'
 });
+var Buffer = require('buffer').Buffer;
+var toArray = require('stream-to-array');
+var toBuffer = require('stream-to-buffer');
+var fs = require('fs');
 
 module.exports = {
     upload: function(req, res) {
-        var filename = ''
-        var dataset = req.param('dataset');
-        if (!shortid.isValid(dataset)) return res.badRequest('Dataset can contain only numbers and letters');
-        UploadService.uploadFile(req, res, filename);
-
-        var data = actionUtil.parseValues(req)
-        data.url = sails.config.odin.uploadFolder + '/' + dataset + '/' + filename
-        File.create(data).exec(function created(err, newInstance) {
-
-            if (err) return res.negotiate(err);
-
-            if (req._sails.hooks.pubsub) {
-                if (req.isSocket) {
-                    Model.subscribe(req, newInstance);
-                    Model.introduce(newInstance);
-                }
-                // Make sure data is JSON-serializable before publishing
-                var publishData = _.isArray(newInstance) ?
-                    _.map(newInstance, function(instance) {
-                        return instance.toJSON();
-                    }) :
-                    newInstance.toJSON();
-                Model.publishCreate(publishData, !req.options.mirror && req);
-            }
-
-            // Send JSONP-friendly response if it's supported
-            res.created(newInstance);
-        });
-
+        UploadService.uploadFile(req, res);
     },
     download: function(req, res) {
         const pk = actionUtil.requirePk(req);
@@ -68,11 +45,26 @@ module.exports = {
                 }
             }).fail(function(err) {
                 console.log(err)
-                res.negotiate()
+                return res.negotiate()
             })
         }).fail(function(err) {
             console.log(err)
-            res.negotiate()
+            return res.negotiate()
         })
+
+
+        // File.findOne(pk).exec(function(err, file) {
+        //     if (err) return res.negotiate(err)
+        //         // Filetype.findOne(pk)
+        //     res.set('Content-Type', mime.lookup(file.name.split('.').pop()));
+        //     res.set('Content-Disposition', 'attachment; filename=' + file.name);
+        //     var dirname = require('path').resolve(sails.config.odin.uploadFolder + '/' + file.dataset + '/' + file.name);
+        //     console.log(dirname);
+        //     var SkipperDisk = require('skipper-disk');
+        //     var fileAdapter = SkipperDisk();
+        //     fileAdapter.read(dirname).on('error', function(err) {
+        //         return res.serverError(err);
+        //     }).pipe(res);
+        // });
     },
 };
--- api/services/UploadService.js
+++ api/services/UploadService.js
@@ -1,9 +1,32 @@
+/*
+ * This service handles the file uploads. It also checks if the file is a text file, and if so encodes it using the default specified in config/odin.js
+ * And, if the file contents can be exposed via the API, inserts it into MongoDB.
+ */
+
+const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
+
+const fs = require('fs');
+const path = require('path');
+const shortid = require('shortid');
+const mime = require('mime');
+const Converter = require("csvtojson").Converter;
+const converter = new Converter({
+    delimiter: 'auto'
+});
+const iconv = require('iconv-lite');
+
 module.exports = {
-    uploadFile: function(req, res, filename) {
+    uploadFile: function(req, res) {
         var extension = '';
+        var filename = '';
         var uploadFile = req.file('uploadFile').on('error', function(err) {
             if (!res.headersSent) return res.negotiate(err);
         });
+        var dataset = req.param('dataset');
+        if (!shortid.isValid(dataset)) return res.badRequest('Dataset can contain only numbers and letters');
+
+        console.log("isNoop: " + uploadFile.isNoop);
+
         if (!uploadFile.isNoop) {
 
             uploadFile.upload({
@@ -17,14 +40,18 @@ module.exports = {
                             return res.badRequest('filetype not allowed');
                         } else {
                             filename = file.filename;
+                            console.log('before cb');
                             cb(null, file.filename);
                         }
                     },
                     dirname: require('path').resolve(sails.config.odin.uploadFolder + '/' + dataset),
-                    maxBytes: 2000 * 1000 * 1000,
+                    maxBytes: 2000000000,
 
                 },
                 function onUploadComplete(err, files) {
+                    console.log('on upload complete');
+                    console.log(files.length);
+                    console.log(files);
                     //	IF ERROR Return and send 500 error with error
                     if (err) return res.serverError(err);
                     if (files.length === 0) {
@@ -43,8 +70,9 @@ module.exports = {
                                 if (extension == 'text/csv') {
                                     converter.fromString(result, function(err, json) {
                                         if (err) {
-                                            res.negotiate(err);
+                                            return res.negotiate(err);
                                         }
+                                        if (json.length === 0) return res.badRequest("Invalid or empty csv.");
                                         // Retrieve
                                         var MongoClient = require('mongodb').MongoClient;
 
@@ -64,12 +92,58 @@ module.exports = {
                                     });
                                 }
 
-                                fs.writeFile(filePath, withBOM, function() {
-                                    console.log("Done!!");
-                                });
+                                fs.writeFile(filePath, withBOM, function() {});
                             });
+
+                        var data = actionUtil.parseValues(req)
+                        File.create(data).exec(function created(err, newInstance) {
+                            if (err) return res.negotiate(err);
+
+                            if (req._sails.hooks.pubsub) {
+                                if (req.isSocket) {
+                                    Model.subscribe(req, newInstance);
+                                    Model.introduce(newInstance);
+                                }
+                                // Make sure data is JSON-serializable before publishing
+                                var publishData = _.isArray(newInstance) ?
+                                    _.map(newInstance, function(instance) {
+                                        return instance.toJSON();
+                                    }) :
+                                    newInstance.toJSON();
+                                Model.publishCreate(publishData, !req.options.mirror && req);
+                            }
+
+                            // Send JSONP-friendly response if it's supported
+                            res.created(newInstance);
+                        });
                     }
-                })
+                }
+            )
+        } else {
+            return res.badRequest('No file was uploaded.');
         }
+        /*
+        var extension = '';
+        var filename = '';
+        var uploadFile = req.file('uploadFile').on('error', function(err) {
+            console.log("uploadFile err: " + err);
+            if (!res.headersSent) return res.negotiate(err);
+        });
+        var dataset = req.param('dataset');
+
+        if (!shortid.isValid(dataset)) return res.badRequest('Dataset can contain only numbers and letters');
+
+        if (!uploadFile.isNoop) {
+            uploadFile.upload({
+                saveAs: function(file, cb) {
+                    //Get the extension of the file
+                    extension = mime.lookup(file.filename.split('.').pop());
+                    checkExtension(file, extension, cb);
+                },
+                dirname: path.resolve(sails.config.odin.uploadFolder + '/' + dataset),
+                maxBytes: 2000 * 1000 * 1000
+            }, onUploadComplete(err, files, filename));
+        }
+        */
     }
 }
