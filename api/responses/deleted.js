51157999550d8a26ec2dba9c821a0d77036d9cf6
Rita Zerrizuela <zeta@widcket.com>
2016-06-09 10:11:37 -0300

    Made DELETE response empty

--- api/responses/deleted.js
+++ api/responses/deleted.js
@@ -1,25 +1,18 @@
 "use strict";
 
 /**
- * 200 (OK DELETED) Response
+ * 206 (OK DELETED) Response
  *
- * The request has been fulfilled and resulted in a new resource being deleted.
+ * The request has been fulfilled and resulted in a resource being deleted.
  */
 
 module.exports = function(data, config) {
-    const response = _.assign({
-        meta: _.get(config, 'meta', {}),
-        links: _.get(config, 'links', {}),
-    });
     //
     //     code: _.get(config, 'code', 'CREATED'),
-    //     message: _.get(config, 'message', 'The request has been fulfilled and resulted in a new resource being created'),
+    //     message: _.get(config, 'message', 'The request has been fulfilled and resulted in a resource being created'),
     //     data: data || {}
     // }, _.get(config, 'root', {}));
 
-    this.res.set({
-        'Content-Type': 'application/json',
-    });
-    this.res.status(200);
-    this.res.send(response);
+    this.res.status(206);
+    this.res.end();
 };
