d3d20ab3e2a156d4676053006d11e0771d7ba29b
Rita Zerrizuela <zeta@widcket.com>
2016-06-14 11:25:19 -0300

    Commented other ResponseGET methods

--- api/services/ResponseBuilderService.js
+++ api/services/ResponseBuilderService.js
@@ -300,6 +300,9 @@ class ResponseGET extends ResponseBuilder {
         }
     }
 
+    /*
+     * Parses the 'sort' query param and builds an object with it
+     */
     parseSort(req) {
         var sort = req.param('sort') || req.options.sort;
         var orderBy = req.param('orderBy') || req.options.orderBy;
@@ -311,12 +314,15 @@ class ResponseGET extends ResponseBuilder {
         };
     }
 
+    /*
+     * Parses the 'include' query param and builds an object with it, to be consumed by populate()
+     */
     parseInclude(req) {
         var includes = this.req.param('include') ? this.req.param('include').replace(/ /g, '').split(',') : [];
         var splits = [];
         var results = {
-            full: [],
-            partials: {}
+            full: [], // Here go the models that will be included with all their attributes
+            partials: {} // Here, the models that will be included with only the specified attributes. Each model is a key holding an array of attributes.
         };
 
         if (includes.length > 0) {
@@ -338,6 +344,9 @@ class ResponseGET extends ResponseBuilder {
         return results;
     }
 
+    /*
+     * Handles the population of related items and collections
+     */
     populate(query, model, includes) {
         // Fully populate non collection items
         _.forEach(model.definition, function(value, key) {
@@ -357,7 +366,7 @@ class ResponseGET extends ResponseBuilder {
             });
         }, this);
 
-        // Currently partial includes are supported in Waterline, but are adapter-dependant
+        // Partial includes are supported in Waterline, but are adapter-dependant
         // Since not many adapters implement them we're doing it by hand
         // TODO: Check if the adapter supports them, to avoid the heavy load of the custom solution
         // Fully populate included partials (will be filtered out later)
