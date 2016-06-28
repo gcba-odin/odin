c8cf5477b5d2fc55887e50822c1eb008f0b37c2a
Rita Zerrizuela <zeta@widcket.com>
2016-06-28 15:44:44 -0300

    Added validation

--- api/models/FileType.js
+++ api/models/FileType.js
@@ -26,6 +26,7 @@ module.exports = {
         name: {
             type: 'string',
             required: true,
+            unique: true,
             size: 150,
             minLength: 1
         },
