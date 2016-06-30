234e1799b40143152f874f64fef58ad522bf7778
arielleguizamon <ariel.leguizamon@gmail.com>
2016-06-30 11:12:26 -0300

    Added 410 gone response

---
+++ api/responses/gone.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * 410 (Gone) Response
+ *
+ * The requested resource is soft deleted
+ *
+ */
+
+module.exports = function(data, config) {
+    const response = _.assign({
+        code: _.get(config, 'code', 'E_GONE'),
+        message: _.get(config, 'message', 'The requested resource is not available anymore.')
+    }, _.get(config, 'root', {}));
+
+    this.res.set({
+        'Content-Type': 'application/json'
+    });
+    this.res.status(410);
+    this.res.send(response);
+};
