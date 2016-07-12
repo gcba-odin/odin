12ae98da31dcd59db0c128f3f3a4de66883621fd
arielleguizamon <ariel.leguizamon@gmail.com>
2016-07-12 09:44:25 -0300

    changed update data response from array to objetc

--- api/blueprints/update.js
+++ api/blueprints/update.js
@@ -11,13 +11,10 @@ const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
  */
 module.exports = (req, res) => {
     var builder = new Response.ResponsePATCH(req, res);
-
     builder.update
         .then(record => {
             var model = (actionUtil.parseModel(req)).adapter.identity;
-
             if (_.isUndefined(record[0])) {
-
                 LogService.winstonLog('error', model + ' not found', {
                     ip: req.ip
                 });
@@ -46,9 +43,9 @@ module.exports = (req, res) => {
             //populate the response
             builder._model.find(record[0].id).populate(associations).exec(function(err, record) {
                 if (err) return res.negotiate(err);
-                res.updated(record, {
-                    meta: builder.meta(record),
-                    links: builder.links(record)
+                res.updated(record[0], {
+                    meta: builder.meta(record[0]),
+                    links: builder.links(record[0])
                 });
 
             });
