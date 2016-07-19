ca6b746950b11521d95637fe85a559c1bd5e5874
Rita Zerrizuela <zeta@widcket.com>
2016-07-19 17:28:50 -0300

    Fixed url issue

--- api/services/ResponseBuilderService.js
+++ api/services/ResponseBuilderService.js
@@ -368,12 +368,12 @@ class ResponseGET extends ResponseBuilder {
 
         if (this._many) {
             // Check if no parameters given
-            var params = (!_.isEmpty(this.requestQuery));
+            var params = !_.isEmpty(this.req.query);
+            console.log(params);
             // If we have &skip or ?skip, we delete it from the url
             var url = this.req.url.replace(/.skip=\d+/g, "");
 
-            const _baseLinkToModel = sails.config.odin.baseUrl + url +
-                ((params || this.params.page <= this.params.pages) ? '?' : '&');
+            const _baseLinkToModel = sails.config.odin.baseUrl + url + (params && url.indexOf('?') === -1 ? '?' : '&');
 
             const _linkToModel = _baseLinkToModel + 'skip=';
 
