1a8f3873e02f81c216cb9979a96911f8fb0b4f41
Rita Zerrizuela <zeta@widcket.com>
2016-07-19 12:11:33 -0300

    Updated configs

--- config/odin.js
+++ config/odin.js
@@ -1,18 +1,10 @@
-module.exports.odin = {
-    uploadFolder: '/home/Admin001/files',
+const path = require('path');
 
-    allowedTypes: ['text/yaml', 'text/csv', 'text/html', 'text/calendar',
-        'application/pdf', 'application/x-rar-compressed', 'application/rss+xml',
-        'application/octet-stream', 'application/vnd.ms-excel', 'application/xml',
-        'application/zip', 'application/vnd.ms-excel',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    ],
+module.exports.odin = {
+    uploadFolder: path.join(process.cwd(), 'files'),
     defaultEncoding: 'utf8',
     dataStorage: {
         host: 'localhost',
         port: '27017'
-    },
-
-    logWhitelist: ['category', 'dataset', 'fileType', 'file',
-        'organization', 'status', 'tag', 'updateFrequency', 'user']
+    }
 };
