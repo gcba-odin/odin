e898a563528e170475811758fa637d9489a1ed33
arielleguizamon <ariel.leguizamon@gmail.com>
2016-07-22 12:12:58 -0300

    if kong headers is set, authentication is skipped

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -8,10 +8,11 @@
 const passport = require('passport');
 
 module.exports = (req, res, next) => {
-    passport.authenticate('jwt', (error, user, info) => {
-        if (error || !user) return res.negotiate(error || info);
+    if (!req.get('X-Consumer-ID') || req.host !== sails.config.odin.kongHost)
+        passport.authenticate('jwt', (error, user, info) => {
+            if (error || !user) return res.negotiate(error || info);
 
-        req.user = user;
-        next();
-    })(req, res);
+            req.user = user;
+            next();
+        })(req, res);
 };
