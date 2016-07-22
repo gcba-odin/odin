1a07be7d3fd5ea884e6e44a5fbf623f4e7303ea9
Rita Zerrizuela <zeta@widcket.com>
2016-07-22 17:39:59 -0300

    Added statistics tests

---
+++ test/unit/controllers/StatisticController.test.js
@@ -0,0 +1,441 @@
+//-- test/unit/controllers/StatisticController.test.js
+'use strict';
+
+require('sails-test-helper');
+
+const chai = require('chai');
+const assert = chai.assert;
+const shortid = require('shortid');
+var statisticId;
+
+chai.use(require('chai-fs'));
+chai.use(require('chai-string'));
+
+
+/*
+ * All Statistics
+ */
+
+describe('All Statistics', function() {
+    describe('- GET /statistics', function() {
+        it('- Should get all statistics', function(done) {
+            request.get('/statistics')
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    // Meta
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'OK');
+
+                    assert.property(result.body.meta, 'count');
+                    assert.isNumber(result.body.meta.count);
+
+                    assert.property(result.body.meta, 'limit');
+                    assert.isNumber(result.body.meta.limit);
+
+                    assert.property(result.body.meta, 'start');
+                    assert.isNumber(result.body.meta.start);
+
+                    assert.property(result.body.meta, 'end');
+                    assert.isNumber(result.body.meta.end);
+
+                    assert.property(result.body.meta, 'page');
+                    assert.isNumber(result.body.meta.page);
+
+                    assert.property(result.body.meta, 'pages');
+                    assert.isNumber(result.body.meta.pages);
+
+                    assert.isAtMost(result.body.meta.end, result.body.meta.count);
+                    assert.isAtMost(result.body.meta.page, result.body.meta.pages);
+
+                    // Data
+                    assert.property(result.body, 'data');
+                    assert.isArray(result.body.data);
+
+                    // Links
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'firstItem');
+                    assert.isString(result.body.links.firstItem);
+                    assert.endsWith(result.body.links.firstItem, '/statistics/first');
+
+                    assert.property(result.body.links, 'lastItem');
+                    assert.isString(result.body.links.lastItem);
+                    assert.endsWith(result.body.links.lastItem, '/statistics/last');
+
+                    if (result.body.data.length > 0) {
+                        result.body.data.forEach(function(element) {
+                            assert.property(element, 'id');
+                            assert.isString(element.id);
+                            assert.ok(shortid.isValid(element.id));
+
+                            assert.property(element, 'name');
+                            assert.isString(element.name);
+
+                            assert.property(element, 'description');
+                            if (element.description) assert.isString(element.description);
+
+                            assert.property(element, 'createdBy');
+
+                            assert.property(element, 'createdAt');
+                            assert.property(element, 'updatedAt');
+                        }, this);
+                    }
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    // Pagination
+
+    describe('- GET /statistics?limit=2', function() {
+        it('- Should get the first two statistics', function(done) {
+            request.get('/statistics?limit=2')
+                .set('Accept', 'application/json')
+                .expect(206)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    // Meta
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'PARTIAL_CONTENT');
+
+                    assert.property(result.body.meta, 'count');
+                    assert.isNumber(result.body.meta.count);
+
+                    assert.property(result.body.meta, 'limit');
+                    assert.isNumber(result.body.meta.limit);
+                    assert.equal(result.body.meta.limit, 2);
+
+                    assert.property(result.body.meta, 'start');
+                    assert.isNumber(result.body.meta.start);
+                    assert.equal(result.body.meta.start, 1);
+
+                    assert.property(result.body.meta, 'end');
+                    assert.isNumber(result.body.meta.end);
+                    assert.equal(result.body.meta.end, 2);
+
+                    assert.property(result.body.meta, 'page');
+                    assert.isNumber(result.body.meta.page);
+                    assert.equal(result.body.meta.page, 1);
+
+                    assert.property(result.body.meta, 'pages');
+                    assert.isNumber(result.body.meta.pages);
+
+                    assert.isAtMost(result.body.meta.end, result.body.meta.count);
+                    assert.isAtMost(result.body.meta.page, result.body.meta.pages);
+
+                    // Data
+                    assert.property(result.body, 'data');
+                    assert.isArray(result.body.data);
+                    assert.lengthOf(result.body.data, 2);
+
+                    // Links
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'next');
+                    assert.isString(result.body.links.next);
+                    assert.endsWith(result.body.links.next, 'statistics?limit=2&skip=2');
+
+                    assert.property(result.body.links, 'last');
+                    assert.isString(result.body.links.last);
+                    assert.endsWith(result.body.links.last, 'statistics?limit=2&skip=8');
+
+                    assert.property(result.body.links, 'firstItem');
+                    assert.isString(result.body.links.firstItem);
+                    assert.endsWith(result.body.links.firstItem, '/statistics/first');
+
+                    assert.property(result.body.links, 'lastItem');
+                    assert.isString(result.body.links.lastItem);
+                    assert.endsWith(result.body.links.lastItem, '/statistics/last');
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    describe('- GET /statistics?limit=2&skip=2', function() {
+        it('- Should get the next page', function(done) {
+            request.get('/statistics?limit=2&skip=2')
+                .set('Accept', 'application/json')
+                .expect(206)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'PARTIAL_CONTENT');
+
+                    assert.property(result.body.meta, 'count');
+                    assert.isNumber(result.body.meta.count);
+
+                    assert.property(result.body.meta, 'limit');
+                    assert.isNumber(result.body.meta.limit);
+                    assert.equal(result.body.meta.limit, 2);
+
+                    assert.property(result.body.meta, 'start');
+                    assert.isNumber(result.body.meta.start);
+                    assert.equal(result.body.meta.start, 3);
+
+                    assert.property(result.body.meta, 'end');
+                    assert.isNumber(result.body.meta.end);
+                    assert.equal(result.body.meta.end, 4);
+
+                    assert.property(result.body.meta, 'page');
+                    assert.isNumber(result.body.meta.page);
+                    assert.equal(result.body.meta.page, 2);
+
+                    assert.property(result.body.meta, 'pages');
+                    assert.isNumber(result.body.meta.pages);
+
+                    assert.isAtMost(result.body.meta.end, result.body.meta.count);
+                    assert.isAtMost(result.body.meta.page, result.body.meta.pages);
+
+                    // Data
+                    assert.property(result.body, 'data');
+                    assert.isArray(result.body.data);
+                    assert.lengthOf(result.body.data, 2);
+
+                    // Links
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'previous');
+                    assert.isString(result.body.links.previous);
+                    assert.endsWith(result.body.links.previous, 'statistics?limit=2&skip=0');
+
+                    assert.property(result.body.links, 'next');
+                    assert.isString(result.body.links.next);
+                    assert.endsWith(result.body.links.next, 'statistics?limit=2&skip=4');
+
+                    assert.property(result.body.links, 'first');
+                    assert.isString(result.body.links.first);
+                    assert.endsWith(result.body.links.first, 'statistics?limit=2&skip=0');
+
+                    assert.property(result.body.links, 'last');
+                    assert.isString(result.body.links.last);
+                    assert.endsWith(result.body.links.last, 'statistics?limit=2&skip=8');
+
+                    assert.property(result.body.links, 'firstItem');
+                    assert.isString(result.body.links.firstItem);
+                    assert.endsWith(result.body.links.firstItem, '/statistics/first');
+
+                    assert.property(result.body.links, 'lastItem');
+                    assert.isString(result.body.links.lastItem);
+                    assert.endsWith(result.body.links.lastItem, '/statistics/last');
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    describe('- GET /statistics?limit=2&skip=8', function() {
+        it('- Should get the last statistic', function(done) {
+            request.get('/statistics?limit=2&skip=8')
+                .set('Accept', 'application/json')
+                .expect(206)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'PARTIAL_CONTENT');
+
+                    assert.property(result.body.meta, 'count');
+                    assert.isNumber(result.body.meta.count);
+
+                    assert.property(result.body.meta, 'limit');
+                    assert.isNumber(result.body.meta.limit);
+                    assert.equal(result.body.meta.limit, 2);
+
+                    assert.property(result.body.meta, 'start');
+                    assert.isNumber(result.body.meta.start);
+                    assert.equal(result.body.meta.start, 9);
+
+                    assert.property(result.body.meta, 'end');
+                    assert.isNumber(result.body.meta.end);
+                    assert.equal(result.body.meta.end, 9);
+
+                    assert.property(result.body.meta, 'page');
+                    assert.isNumber(result.body.meta.page);
+                    assert.equal(result.body.meta.page, 5);
+
+                    assert.property(result.body.meta, 'pages');
+                    assert.isNumber(result.body.meta.pages);
+
+                    assert.isAtMost(result.body.meta.end, result.body.meta.count);
+                    assert.isAtMost(result.body.meta.page, result.body.meta.pages);
+
+                    // Data
+                    assert.property(result.body, 'data');
+                    assert.isArray(result.body.data);
+
+                    // Links
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'previous');
+                    assert.isString(result.body.links.previous);
+                    assert.endsWith(result.body.links.previous, 'statistics?limit=2&skip=6');
+
+                    assert.property(result.body.links, 'first');
+                    assert.isString(result.body.links.first);
+                    assert.endsWith(result.body.links.first, 'statistics?limit=2&skip=0');
+
+                    assert.property(result.body.links, 'firstItem');
+                    assert.isString(result.body.links.firstItem);
+                    assert.endsWith(result.body.links.firstItem, '/statistics/first');
+
+                    assert.property(result.body.links, 'lastItem');
+                    assert.isString(result.body.links.lastItem);
+                    assert.endsWith(result.body.links.lastItem, '/statistics/last');
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    // Get an inexistent relation for an inexistent item
+    describe('- GET /statistics/:fakeId/arandomstring', function() {
+        it('- Should get 404 Not Found error', function(done) {
+            request.get('/statistics/fakeId/arandomstring')
+                .set('Accept', 'application/json')
+                .expect(404)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'E_NOT_FOUND');
+
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'entryPoint');
+                    assert.isString(result.body.links.entryPoint);
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    // 501 Not Implemented Errors
+
+    describe('- POST /statistics', function() {
+        it('- Should get 501 Method Not Implemented error', function(done) {
+            request.post('/statistics')
+                .set('Accept', 'application/json')
+                .expect(501)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'E_NOT_IMPLEMENTED');
+
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'all');
+                    assert.isString(result.body.links.all);
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    describe('- DELETE /statistics', function() {
+        it('- Should get 501 Method Not Implemented error', function(done) {
+            request.del('/statistics')
+                .set('Accept', 'application/json')
+                .expect(501)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'E_NOT_IMPLEMENTED');
+
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'all');
+                    assert.isString(result.body.links.all);
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    describe('- PATCH /statistics', function() {
+        it('- Should get 501 Method Not Implemented error', function(done) {
+            request.patch('/statistics')
+                .set('Accept', 'application/json')
+                .expect(501)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'E_NOT_IMPLEMENTED');
+
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'all');
+                    assert.isString(result.body.links.all);
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+
+    describe('- PUT /statistics', function() {
+        it('- Should get 501 Method Not Implemented error', function(done) {
+            request.put('/statistics')
+                .set('Accept', 'application/json')
+                .expect(501)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .end(function(err, result) {
+                    assert.property(result.body, 'meta');
+                    assert.isObject(result.body.meta);
+
+                    assert.property(result.body.meta, 'code');
+                    assert.isString(result.body.meta.code);
+                    assert.equal(result.body.meta.code, 'E_NOT_IMPLEMENTED');
+
+                    assert.property(result.body, 'links');
+                    assert.isObject(result.body.links);
+
+                    assert.property(result.body.links, 'all');
+                    assert.isString(result.body.links.all);
+
+                    err ? done(err) : done();
+                });
+        });
+    });
+});
