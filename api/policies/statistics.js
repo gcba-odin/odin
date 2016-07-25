e16b53a1a7e826497178c73b434a8062a00f242d
arielleguizamon <ariel.leguizamon@gmail.com>
2016-07-25 15:34:03 -0300

    statistics test

--- api/policies/statistics.js
+++ api/policies/statistics.js
@@ -4,6 +4,10 @@ var ipaddr = require('ipaddr.js');
 module.exports = (req, res, next) => {
     //TBD: Client with KONG!
     var addr = ipaddr.parse(req.ip);
+    console.log('is ipv4mapped addres ? ' + addr.isIPv4MappedAddress());
+    console.log('addr to ipv4 -> to string =  ' + addr.toIPv4Address().toString());
+    console.log('addr to string =  ' + addr.toString());
+
     data = {
         ip: addr.isIPv4MappedAddress() ? addr.toIPv4Address().toString() : addr.toString(),
         method: req.method,
