efb88975e3d54895f3c529e391e839bf2611dbad
Rita Zerrizuela <zeta@widcket.com>
2016-08-02 09:19:27 -0300

    Added link field to Chart

--- api/models/Chart.js
+++ api/models/Chart.js
@@ -43,6 +43,11 @@ module.exports = {
             url: true,
             size: 500
         },
+        link: {
+            type: 'string',
+            url: true,
+            size: 500
+        },
         embedCode: {
             type: 'string',
             size: 500
