bcb19de84b9ef8cf49d7ed04ec69dbb7308c760e
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-08-10 17:34:25 -0300

    working on kong refresh token

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,7 @@
  */
 const _ = require('lodash');
 const passport = require('passport');
+var requestify = require('requestify');
 
 module.exports = {
 
@@ -23,17 +24,31 @@ module.exports = {
      * @param req
      * @param res
      */
+    // refreshToken(req, res) {
+    //     if (!req.param('token')) return res.badRequest(null, {
+    //         message: 'You must provide token parameter'
+    //     });
+    //
+    //     const oldDecoded = CipherService.jwt.decodeSync(req.param('token'));
+    //
+    //     res.ok({
+    //         token: CipherService.jwt.encodeSync({
+    //             id: oldDecoded.id
+    //         })
+    //     });
+    // }
     refreshToken(req, res) {
-        if (!req.param('token')) return res.badRequest(null, {
-            message: 'You must provide token parameter'
-        });
-
-        const oldDecoded = CipherService.jwt.decodeSync(req.param('token'));
-
-        res.ok({
-            token: CipherService.jwt.encodeSync({
-                id: oldDecoded.id
+        console.log('before post')
+        requestify.post(sails.config.odin.kongAdmin, {
+                consumer_id: '7fb5addf-5263 - 4a28-91dd-8f9bbc44ab16',
+                created_at: Date.now(),
+                id: '633cc788-2568-4254-84d8-2871cad8efff',
+                key: '99afc57fdbc04c6cb2dd4ba5690e4346',
+                secret: 'dcc01414b70e4f45aa2371b9b82ef95e'
             })
-        });
+            .then(function (response) {
+                // Get the response body
+                console.dir(response.getBody());
+            });
     }
 };
