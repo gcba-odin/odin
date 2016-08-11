a6fce8a5b675d0703d9fea350dcfca1b7ba2c2f6
arielleguizamon <ariel.leguizamon@gmail.com>
2016-08-11 10:43:34 -0300

    update statistics endpoint

--- api/blueprints/statistics.js
+++ api/blueprints/statistics.js
@@ -8,6 +8,7 @@
 const _ = require('lodash');
 const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
 const pluralize = require('pluralize');
+const shortid = require('shortid');
 
 
 module.exports = (req, res) => {
@@ -24,18 +25,39 @@ module.exports = (req, res) => {
         all: '/' + modelPlural,
         first: '/' + modelPlural + '/first',
         last: '/' + modelPlural + '/last',
-        count: '/' + modelPlural + '/count',
+        count: '/' + modelPlural + '/count'
     };
+
+    var filtersValues = _.values(filters)
 
     Statistic.find({
         resource: model
-    }).exec(function (err, data) {
-        var groupedData = _.groupBy(data, function (value) {
+    }).exec(function(err, data) {
+        var groupedData = _.groupBy(data, function(value) {
             return value.endpoint;
         });
+        var countedData = _.transform(groupedData, function(result, val, key) {
 
-        var countedData = _.transform(groupedData, function (result, val, key) {
-            result[key] = _.size(val);
+            // Group methods {GET: 20, OPTIONS: 5}
+            var groupedMethods = _.groupBy(val, function(value) {
+                return value.method;
+            });
+            groupedMethods = _.transform(groupedMethods, function(result, val, key) {
+                result[key] = _.size(val);
+            }, {});
+            // if the key if one of the generals (filters)
+            if (filtersValues.indexOf(key) === -1) {
+                var itemId = _.split(key, '/')[2];
+                // else, we return an object
+                var value = {
+                    count: groupedMethods,
+                    item: shortid.isValid(itemId) ? itemId : '',
+                    resource: _.first(val).resource
+                }
+                result[key] = value;
+            } else {
+                result[key] = _.size(val);
+            }
         }, {});
 
 
