3397d0a3593f76995d1fba7a091005df0ced5ada
arielleguizamon <ariel.leguizamon@gmail.com>
2016-08-16 15:36:43 -0300

    fix isAuthenticated policy

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -8,11 +8,14 @@
 const passport = require('passport');
 
 module.exports = (req, res, next) => {
-    if (!req.get('X-Consumer-ID') || req.host !== sails.config.odin.kongHost)
+    if (!req.get('x-consumer-id') || req.host !== sails.config.odin.kongHost)
         passport.authenticate('jwt', (error, user, info) => {
             if (error || !user) return res.negotiate(error || info);
 
             req.user = user;
             next();
         })(req, res);
+    else {
+        next();
+    }
 };
