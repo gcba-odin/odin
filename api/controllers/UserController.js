689369ea1d6b48c00b753f29eac16c97b4cd8564
arielleguizamon <ariel.leguizamon@gmail.com>
2016-08-17 08:56:30 -0300

    hard coded token

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -41,18 +41,19 @@ module.exports = {
     refreshToken(req, res) {
         var consumerId = req.param('consumerId');
         var consumerUsername = req.param('consumer')
-        requestify.post(sails.config.odin.kongAdmin + '/consumers/' + consumerUsername + '/jwt', {
-                consumer_id: consumerId
-            })
-            .then(function(response) {
-                // Get the response body
-                var credential = response.getBody();
-                var payload = {
-                    iss: credential.key
-                };
-                var secret = credential.secret
-                var token = jwt.encode(payload, secret);
-                return res.ok(token)
-            });
+        return res.ok('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5OWFmYzU3ZmRiYzA0YzZjYjJkZDRiYTU2OTBlNDM0NiJ9.Uo0I98Fu3BX8XlOgSnIvfeFx2Z_LdqM8WNT4hSMdDDM')
+            // requestify.post(sails.config.odin.kongAdmin + '/consumers/' + consumerUsername + '/jwt', {
+            //         consumer_id: consumerId
+            //     })
+            //     .then(function(response) {
+            //         // Get the response body
+            //         var credential = response.getBody();
+            //         var payload = {
+            //             iss: credential.key
+            //         };
+            //         var secret = credential.secret
+            //         var token = jwt.encode(payload, secret);
+            //         return res.ok(token)
+            //     });
     }
 };
