36c48977daf7d72c27985c1184382b18f3220362
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-08-18 08:57:46 -0300

    unique name on entities

--- api/models/Config.js
+++ api/models/Config.js
@@ -8,61 +8,62 @@
 var shortId = require('shortid');
 
 module.exports = {
-  schema: true,
+    schema: true,
 
-  attributes: {
-    id: {
-      type: 'string',
-      unique: true,
-      index: true,
-      defaultsTo: shortId.generate,
-      primaryKey: true,
-      size: 15
-    },
-    description: {
-      type: 'string',
-      required: true,
-      size: 250,
-      minLength: 3
-    },
-    type: {
-      type: 'string',
-      required: true,
-      enum: ['bool', 'string', 'int', 'float']
-    },
-    key: {
-      type: 'string',
-      required: true,
-      size: 100,
-      minLength: 1
-    },
-    value: {
-      type: 'string',
-      size: 250
-    },
-    model: {
-      type: 'string',
-      size: 50
-    },
-    updatedBy: {
-      model: 'user'
+    attributes: {
+        id: {
+            type: 'string',
+            unique: true,
+            index: true,
+            defaultsTo: shortId.generate,
+            primaryKey: true,
+            size: 15
+        },
+        description: {
+            type: 'string',
+            required: true,
+            unique: true,
+            size: 250,
+            minLength: 3
+        },
+        type: {
+            type: 'string',
+            required: true,
+            enum: ['bool', 'string', 'int', 'float']
+        },
+        key: {
+            type: 'string',
+            required: true,
+            size: 100,
+            minLength: 1
+        },
+        value: {
+            type: 'string',
+            size: 250
+        },
+        model: {
+            type: 'string',
+            size: 50
+        },
+        updatedBy: {
+            model: 'user'
+        },
+
+        toJSON() {
+            return this.toObject();
+        }
     },
 
-    toJSON() {
-      return this.toObject();
+    searchables: ['description'],
+
+    beforeCreate(values, next) {
+        values.updatedBy = values.createdBy;
+        delete values.createdBy;
+        next();
+    },
+    beforeUpdate(values, next) {
+        values.updatedBy = values.createdBy;
+        delete values.createdBy;
+        next();
     }
-  },
-
-  searchables: ['description'],
-
-  beforeCreate(values, next) {
-    values.updatedBy = values.createdBy;
-    delete values.createdBy;
-    next();
-  },
-  beforeUpdate(values, next) {
-    values.updatedBy = values.createdBy;
-    delete values.createdBy;
-    next();
-  }
 };
