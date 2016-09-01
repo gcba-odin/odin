7a1e6c2268295c1d37fa4e8762510b1a7fb4c63c
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-09-01 10:01:51 -0300

    fix charts with comma, and rounded result

--- api/controllers/ChartController.js
+++ api/controllers/ChartController.js
@@ -69,11 +69,13 @@ module.exports = {
                                 var key = value[0][element1];
                                 var val = _.sumBy(value, function(each) {
                                     // in case a number is like 192.123.522, transform it to 192123522
-                                    return _.toNumber(_.join(_.split(each[element2], '.'), ""))
+                                    var withoutDots = _.join(_.split(each[element2], '.'), "");
+                                    // in case a number is like 192123,522, transform it to 192123.522
+                                    return _.toNumber(_.replace(withoutDots, ',', '.'));
+
                                 });
-                                result[key] = val;
+                                result[key] = _.round(val,2);
                             }, {});
-                            console.dir(chartData)
                         }
                     }
                     values.data = {
