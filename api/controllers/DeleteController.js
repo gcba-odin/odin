1f19db10ed9e9d93e50a5add65f7e6dfc7d2ab02
maxi <maxiejbe92@gmail.com>
2016-09-06 13:51:51 -0300

    Hard delete if no model relations

--- api/controllers/DeleteController.js
+++ api/controllers/DeleteController.js
@@ -4,7 +4,10 @@
  * DeleteController
  * @description :: Server-side logic for ...
  */
+const Response = require('../services/ResponseBuilderService');
 const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
+
+var destroy = require('../blueprints/destroy.js');
 
 module.exports = {
     delete: function(req, res) {
@@ -13,7 +16,45 @@ module.exports = {
 
         var user;
         user = (_.isUndefined(req.user) ? 'noUser' : req.user.id);
-
+        
+        var includes = [];
+        _.forEach(model.associations, function (association) {
+            if (association.type === 'collection')
+                includes.push(association.alias);
+        });
+        req.params.include = includes.join(",");
+        
+        var builder = new Response.ResponseGET(req, res, false);
+        builder.findQuery()
+        .then(records => {
+            if (_.isUndefined(records[0])) return res.notFound(null, {
+                meta: builder.meta(undefined),
+                links: builder.links(undefined)
+            });
+            else {
+                var returnRecord = records[0];
+                var hardDelete = true;
+                _.forEach(includes, function (include) {
+                    var associationValue = returnRecord[include];
+                    if(!_.isUndefined(associationValue) && !_.isEmpty(associationValue)){
+                        hardDelete = false;
+                    }
+                });
+                
+                if(hardDelete){
+                    //Model relations are not populated: Hard delete (destroy)
+                    destroy(req,res);    
+                }
+                else{
+                    //Existing model relations: Soft delete
+                    sails.controllers.delete.softDelete(req, res, model, id);
+                }
+            }
+        })
+        .catch(res.negotiate);
+    },
+    softDelete: function(req, res, model, id){
+        //Soft delete
         model.update({
             id: id
         }, {
