17e8c8daf8e998ac336d7fdfccaad8bef5667b48
arielleguizamon <ariel.leguizamon@gmail.com>
2016-09-23 14:58:05 -0300

    added recaptcha

--- config/odin.js
+++ config/odin.js
@@ -5,7 +5,7 @@ module.exports.odin = {
     baseUrl: 'http://localhost:3000',
     kongHost: 'http://kongexample.com',
     kongAdmin: 'http://kongexample.com:8001',
-
+    recaptchaSecret: '6LetbAcUAAAAADUH2850T6PNg_d1EXV04Sv48mZP',
     uploadFolder: 'files',
     datasetZipFolder: 'datasets',
 
