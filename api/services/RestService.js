0a484091e477a3e33487a10646193f148b0322e8
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-09-28 17:19:14 -0300

    added rest service

---
+++ api/services/RestService.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/*
+ This service gets data from a REST service and parse it into saveable data
+ */
+
+var requestify = require('requestify');
+var parseString = require('xml2js').parseString;
+
+module.exports = {
+    getData: function (restService) {
+
+        // Object where all the rest web service options will be stored
+        var requestOptions = {};
+
+        // General parameters
+        requestOptions.params = restService.parameters;
+
+        // username and password
+        if (restService.username && restService.password) {
+            requestOptions.auth = {
+                username: restService.username,
+                password: restService.password
+            };
+        }
+
+        // if token is set, we add it to the options
+        if (restService.token) {
+            requestOptions.headers = {
+                Authorization: restService.token
+            }
+        }
+
+        // make the call to the defined url, sending the parameters made
+        requestify.get(restService.url, requestOptions).then(function (response) {
+            // if is xml we parse it
+            if (response.body.indexOf("<?xml") != -1) {
+                var options = {
+                    ignoreAttrs: true,
+                    explicitArray: false
+                };
+                parseString(response.body, options, function (err, result) {
+                    // To get the route to the data
+                    result = result[restService.dataPath];
+                    return result;
+                })
+            }
+            // if is json directly return it
+            else {
+                response = response.getBody();
+                return _.isUndefined(restService.dataPath) ? response : response[restService.dataPath];
+            }
+        });
+    }
+};
