0f1b302499c8b92713fc428a7a66c795ef21d19a
maxi <maxiejbe92@gmail.com>
2016-10-07 11:20:47 -0300

    Sends web service SOAP request

--- api/services/WebService.js
+++ api/services/WebService.js
@@ -33,7 +33,10 @@ module.exports = {
                             console.log(err);
                         }
                         else{
-                            console.log(result);    
+                            UploadService.uploadServiceFile(file, result, function(err, uploadedFile){
+                                //console.log(err);
+                                //console.log(uploadedFile);
+                            });    
                         }
                     });
                 }
