e09058e191e5b1162dffa35f6480e3df8913e517
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-10-20 16:37:04 -0300

    automatically unpublish associated files/visualizations

--- api/services/PublishService.js
+++ api/services/PublishService.js
@@ -3,15 +3,29 @@
 const pluralize = require('pluralize');
 
 module.exports = {
-    publishModel: function(model, id, requiredStatus, res) {
+    publishModel: function (model, id, requiredStatus, res) {
         Config.findOne({
             key: requiredStatus
-        }).exec(function(err, statusConfig) {
-            if (err) return res.negotiate(err)
+        }).exec(function (err, statusConfig) {
+            if (err) return res.negotiate(err);
+            if (requiredStatus === 'unpublishedStatus') {
+                // associations will return 0,1,2 depending on the model unpublished
+                var associations = this.checkAssociations(model);
+                // if associations is eq to 2, the unpublished model is a dataset
+                // so we need to unpublish the files associated, and theirs visualizations
+                if (associations === 2) {
+                    this.unpublishFiles(id, statusConfig.value);
+                }
+                //    if associations is eq to 1, the unpublished model is a a file
+                //    we need to unpublish their visualization
+                else if (associations === 1) {
+                    this.unpublishVisualizations(id, statusConfig.value)
+                }
+            }
             model.update(id, {
                 status: statusConfig.value,
                 publishedAt: requiredStatus === 'publishedStatus' ? new Date() : null
-            }).exec(function(err, updatedRecord) {
+            }).exec(function (err, updatedRecord) {
                 if (err) return res.negotiate(err);
                 var modelName = pluralize(model.adapter.identity);
                 var meta = {
@@ -27,6 +41,51 @@ module.exports = {
                     links: links
                 });
             });
+        }.bind(this));
+    },
+    unpublishFiles: function (id, configValue) {
+        File.update({dataset: id}, {
+            status: configValue,
+            publishedAt: null
+        }).then(function (files) {
+            _.forEach(files, function (file) {
+                PublishService.unpublishVisualizations(file.id, configValue)
+            });
         });
+
+    },
+    unpublishVisualizations(id, configValue){
+
+        _Map.update({file: id}, {
+            status: configValue,
+            publishedAt: null
+        }).then(function (maps) {
+            }
+        );
+        Chart.update({file: id}, {
+            status: configValue,
+            publishedAt: null
+        }).then(function (charts) {
+        });
+    },
+
+    checkAssociations: function (model) {
+        var value;
+        switch (model.identity) {
+            case 'dataset':
+                value = 2;
+                break;
+            case 'file':
+                value = 1;
+                break;
+            case 'map':
+                value = 0;
+                break;
+            case 'chart':
+                value = 0;
+                break;
+        }
+        return value
     }
 }
+;
