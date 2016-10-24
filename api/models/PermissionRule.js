f5957e9a14159c13144009b728944a9fdb8d3d49
Hernan Pieroni <hernan.pieroni@gmail.com>
2016-10-24 13:32:46 -0300

    Adds validations in PermissionRule model

--- api/models/PermissionRule.js
+++ api/models/PermissionRule.js
@@ -38,5 +38,22 @@ module.exports = {
             type: 'boolean',
             defaultsTo: false
         }
-    }
+    },
+
+    beforeValidate: [
+        function validOwnerAttributeOnCreateAction(permission, next) {
+            const invalidOwnerAttribute = permission.action === actions.CREATE && permission.owner;
+            next(invalidOwnerAttribute ? new Error('Owner property cannot be true in create action permission') : null);
+        },
+        function alredyExists(permission, next) {
+            PermissionRule
+                .findOne({
+                    role: permission.role,
+                    action: permission.action,
+                    model: permission.model,
+                    owner: permission.owner
+                })
+                .then(foundPermission => next(foundPermission ? new Error('This permission rule already exists') : null));
+        }
+    ]
 };
