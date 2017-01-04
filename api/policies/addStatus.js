3a035538083c01b17bb6bb464d4cc11d977ae367
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-01-04 11:31:43 -0300

    fix addStatus policy

--- api/policies/addStatus.js
+++ api/policies/addStatus.js
@@ -11,12 +11,12 @@ module.exports = (req, res, next) => {
     if (req.user.hasRole(roles.GUEST)) {
         req.body.status = sails.config.statuses.draft
         next();
-        else {
-            Config.findOneByKey('defaultStatus')
-                .then(statusConfig => {
-                    req.body.status = statusConfig.value;
-                    next();
-                });
-        }
+    } else {
+        Config.findOneByKey('defaultStatus')
+            .then(statusConfig => {
+                req.body.status = statusConfig.value;
+                next();
+            });
+
     }
 };
