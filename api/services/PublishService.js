525b68fe592c9c3acc7646da9ec5658ef1b7151d
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-01-04 11:20:05 -0300

    updated publish service to work with new status flow

--- api/services/PublishService.js
+++ api/services/PublishService.js
@@ -3,102 +3,116 @@
 const pluralize = require('pluralize');
 
 module.exports = {
-    publishModel: function (model, id, requiredStatus, res) {
-        Config.findOne({
-            key: requiredStatus
-        }).exec(function (err, statusConfig) {
-            if (err) return res.negotiate(err);
-            if (requiredStatus === 'unpublishedStatus' || requiredStatus === 'rejectedStatus') {
-                // associations will return 0,1,2 depending on the model unpublished
-                var associations = this.checkAssociations(model);
-                // if associations is eq to 2, the unpublished model is a dataset
-                // so we need to unpublish the files associated, and theirs visualizations
-                if (associations === 2) {
-                    if (requiredStatus === 'unpublishedStatus') {
-                        this.unpublishFiles(id, statusConfig.value);
-                    } else {
-                        this.rejectFiles(id, statusConfig.value);
-                    }
-                }
-                //    if associations is eq to 1, the unpublished model is a a file
-                //    we need to unpublish their visualization
-                else if (associations === 1) {
-                    if (requiredStatus === 'unpublishedStatus') {
-                        this.unpublishVisualizations(id, statusConfig.value)
-                    } else {
-                        this.rejectVisualizations(id, statusConfig.value);
-                    }
-                }
+    publishModel: function(model, id, requiredStatus, res) {
+        // associations will return 0,1,2 depending on the model unpublished
+        var associations = this.checkAssociations(model);
+        // get the status id
+        var status = sails.config.statuses[requiredStatus];
+
+        console.dir(sails.config.statuses)
+        console.dir(requiredStatus)
+        // if associations is eq to 2, the unpublished model is a dataset
+        // so we need to unpublish the files associated, and theirs visualizations
+        if (associations === 2) {
+            if (requiredStatus === 'unpublished') {
+                this.unpublishFiles(id, status);
+            } else {
+                this.rejectFiles(id, status);
             }
-            model.update(id, {
-                status: statusConfig.value,
-                publishedAt: requiredStatus === 'publishedStatus' ? new Date() : null
-            }).exec(function (err, updatedRecord) {
-                if (err) return res.negotiate(err);
-                var modelName = pluralize(model.adapter.identity);
-                var meta = {
-                    code: sails.config.success.OK.code,
-                    message: sails.config.success.OK.message
-                };
-                var links = {
-                    all: sails.config.odin.baseUrl + '/' + modelName,
-                    record: sails.config.odin.baseUrl + '/' + modelName + '/' + updatedRecord.id
-                };
-                return res.ok(updatedRecord, {
-                    meta: meta,
-                    links: links
-                });
-            });
-        }.bind(this));
-    },
-    unpublishFiles: function (id, configValue) {
-        File.update({ dataset: id }, {
-            status: configValue,
-            publishedAt: null
-        }).then(function (files) {
-            _.forEach(files, function (file) {
-                PublishService.unpublishVisualizations(file.id, configValue)
-            });
-        });
-
-    },
-    unpublishVisualizations(id, configValue) {
-
-        _Map.update({ file: id }, {
-            status: configValue,
-            publishedAt: null
-        }).then(function (maps) {
         }
-            );
-        Chart.update({ file: id }, {
-            status: configValue,
-            publishedAt: null
-        }).then(function (charts) {
+        //    if associations is eq to 1, the unpublished model is a a file
+        //    we need to unpublish their visualization
+        else if (associations === 1) {
+            if (requiredStatus === 'unpublished') {
+                this.unpublishVisualizations(id, status)
+            } else {
+                this.rejectVisualizations(id, status);
+            }
+        }
+
+        var updateValues = {
+            status: status
+        }
+
+        requiredStatus === 'unpublished' ? updateValues.unPublishedAt = new Date() : updateValues.rejectedAt = new Date();
+
+        model.update(id,
+            updateValues
+        ).exec(function(err, updatedRecord) {
+            if (err) return res.negotiate(err);
+            var modelName = pluralize(model.adapter.identity);
+            var meta = {
+                code: sails.config.success.OK.code,
+                message: sails.config.success.OK.message
+            };
+            var links = {
+                all: sails.config.odin.baseUrl + '/' + modelName,
+                record: sails.config.odin.baseUrl + '/' + modelName + '/' + updatedRecord.id
+            };
+            return res.ok(updatedRecord, {
+                meta: meta,
+                links: links
+            });
         });
     },
-    rejectFiles: function (id, configValue) {
-        File.update({ dataset: id }, {
-            status: configValue
-        }).then(function (files) {
-            _.forEach(files, function (file) {
-                PublishService.rejectVisualizations(file.id, configValue)
+    unpublishFiles: function(id, status) {
+        File.update({
+            dataset: id
+        }, {
+            status: status,
+            unPublishedAt: new Date()
+        }).then(function(files) {
+            _.forEach(files, function(file) {
+                PublishService.unpublishVisualizations(file.id, status)
+            });
+        });
+
+    },
+    unpublishVisualizations(id, status) {
+
+        _Map.update({
+            file: id
+        }, {
+            status: status,
+            unPublishedAt: new Date()
+        }).then(function(maps) {});
+        Chart.update({
+            file: id
+        }, {
+            status: status,
+            unPublishedAt: new Date()
+        }).then(function(charts) {});
+    },
+    rejectFiles: function(id, status) {
+        File.update({
+            dataset: id
+        }, {
+            status: status,
+            rejectedAt: new Date()
+        }).then(function(files) {
+            _.forEach(files, function(file) {
+                PublishService.rejectVisualizations(file.id, status)
             });
         });
 
     },
 
-    rejectVisualizations(id, configValue) {
-        _Map.update({ file: id }, {
-            status: configValue
-        }).then(function (maps) {
-        });
-        Chart.update({ file: id }, {
-            status: configValue
-        }).then(function (charts) {
-        });
+    rejectVisualizations(id, status) {
+        _Map.update({
+            file: id
+        }, {
+            status: status,
+            rejectedAt: new Date()
+        }).then(function(maps) {});
+        Chart.update({
+            file: id
+        }, {
+            status: status,
+            rejectedAt: new Date()
+        }).then(function(charts) {});
     },
 
-    checkAssociations: function (model) {
+    checkAssociations: function(model) {
         var value;
         switch (model.identity) {
             case 'dataset':
@@ -116,5 +130,4 @@ module.exports = {
         }
         return value
     }
-}
-    ;
+};
