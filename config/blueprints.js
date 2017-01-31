a888c086412fd6d8d19af4d54e531adc4e5572b6
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-01-31 14:29:01 -0300

    default limit set to 20

--- config/blueprints.js
+++ config/blueprints.js
@@ -73,6 +73,6 @@ module.exports = {
      * The default number of records to show in the response from a "find" action
      * @type {Number}
      */
-    defaultLimit: 50
+    defaultLimit: 20
   }
 };
