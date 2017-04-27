9e77024f11381314e4a533da4c715d700a7c0231
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-04-27 16:23:30 -0300

    working on cateogry downloads

--- api/controllers/StatisticController.js
+++ api/controllers/StatisticController.js
@@ -9,8 +9,53 @@ const _ = require('lodash');
 module.exports = {
     downloads(req, res) {
         Statistic.query('select * from downloads()', function(err, result) {
-            result = result.rows
-            return res.ok(result)
-        });
+            var statistics = result.rows;
+            var rows = {}
+            statistics.forEach(function(dataset) {
+                var datasetId = dataset.dataset[0];
+                var datasetCount = dataset.count;
+                rows[datasetId] = datasetCount
+            });
+
+            var datasetsId = _.keys(rows)
+
+            Dataset.find().populate('categories').where({id: datasetsId}).then(function(datasets) {
+                var total = {}
+                total.total = 0;
+                _.forEach(datasets, function(dataset) {
+                    console.log(rows)
+                    total['total'] += rows[dataset.id]
+                    _.forEach(dataset.categories, function(category) {
+                        if (_.isUndefined(total[category.id])) {
+                            total[category.id] = 0
+                        }
+                        total[category.id] += rows[dataset.id]
+                    })
+                })
+                return res.ok(total)
+            })
+
+            //     Category.find().populate('datasets', 'id').exec(function(err, records) {
+            //         console.log(records)
+            //         var groupped = _.groupBy(records, function(record) {
+            //             return record.category
+            //         });
+            //         console.log(groupped)
+            //         var links = {
+            //             all: sails.config.odin.baseUrl + '/datasets'
+            //         };
+            //
+            //         var meta = {
+            //             code: sails.config.success.OK.code,
+            //             message: sails.config.success.OK.message
+            //         };
+            //         return res.ok(groupedByModel, {
+            //             links: links,
+            //             meta: meta
+            //         })
+            //
+            //     });
+            //
+        })
     }
-};
+}
