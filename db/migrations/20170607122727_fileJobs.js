37be2cb07a482002ccfb01553805f41d379b2270
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-07-12 10:12:37 -0300

    fix migration

--- db/migrations/20170607122727_fileJobs.js
+++ db/migrations/20170607122727_fileJobs.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
         table.string('file').index();
         table.boolean('finish').defaultTo(false);
         table.boolean('new').defaultTo(true);
+        table.boolean('urgent').defaultTo(false);
         table.timestamp('endDate').defaultTo(null);
         table.timestamp('createdAt');
         table.timestamp('updatedAt');
