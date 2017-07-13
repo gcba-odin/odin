649d069c7278b59a796448af1fdb0d1f25188e49
arielleguizamon <ariel.leguizamon93@gmail.com>
2017-07-13 11:23:36 -0300

    fix search with slug

--- api/blueprints/search.js
+++ api/blueprints/search.js
@@ -8,20 +8,27 @@
 const _ = require('lodash');
 const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
 const find = require('../blueprints/find.js');
+const slug = require('slug');
 
 module.exports = (req, res) => {
 
-    const q = req.param('query');
-    if (!q) return res.badRequest(null, {
-        message: 'You should specify a "query" parameter!'
-    });
+    let q = req.param('query');
+    if (!q)
+        return res.badRequest(null, {message: 'You should specify a "query" parameter!'});
 
+    // console.log('q = ', decodeURI(q))
+    q = decodeURI(q)
+    // q = unescape(q)
+    // console.log('slugged q = ', slug(decodeURI(q), {lower: true}).toString())
     var model = actionUtil.parseModel(req);
-    _.forEach(model.definition, function (val, key) {
+    _.forEach(model.definition, function(val, key) {
         if (val.type === 'string' && model.searchables && model.searchables.indexOf(key) !== -1) {
-            req.params[key] = q;
-        }
-    });
+            if (key === 'slug')
+                req.params[key] = slug(q, {lower: true})
+            else
+                req.params[key] = q;
+            }
+        });
 
-    return find(req,res);
+    return find(req, res);
 };
